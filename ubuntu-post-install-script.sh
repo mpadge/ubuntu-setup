35e68b016a07a64ee08acbdb51d2d776fea48105
mpadge <mark.padgham@email.com>
2016-10-29 22:08:43 +0200

    add ubuntu-post-install-script

---
+++ ubuntu-post-install-script.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+# -*- Mode: sh; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Authors:
+#   Sam Hewitt <sam@snwh.org>
+#
+# Description:
+#   A post-installation bash script for Ubuntu
+#
+# Legal Preamble:
+#
+# This script is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This script is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <https://www.gnu.org/licenses/gpl-3.0.txt>
+
+# tab width
+tabs 4
+clear
+
+#----- Import Functions -----#
+
+dir="$(dirname "$0")"
+
+. $dir/functions/check
+. $dir/functions/cleanup
+. $dir/functions/configure
+. $dir/functions/packages
+. $dir/functions/thirdparty
+. $dir/functions/nonapt
+. $dir/functions/update
+
+#----- Fancy Messages -----#
+show_error(){
+echo -e "\033[1;31m$@\033[m" 1>&2
+}
+show_info(){
+echo -e "\033[1;32m$@\033[0m"
+}
+show_warning(){
+echo -e "\033[1;33m$@\033[0m"
+}
+show_question(){
+echo -e "\033[1;34m$@\033[0m"
+}
+show_success(){
+echo -e "\033[1;35m$@\033[0m"
+}
+show_header(){
+echo -e "\033[1;36m$@\033[0m"
+}
+show_listitem(){
+echo -e "\033[0;37m$@\033[0m"
+}
+
+# Main
+function main {
+    eval `resize`
+    MAIN=$(whiptail \
+        --notags \
+        --title "Ubuntu Post-Install Script" \
+        --menu "\nWhat would you like to do?" \
+        --cancel-button "Quit" \
+        $LINES $COLUMNS $(( $LINES - 12 )) \
+        update      'Perform system update' \
+        packages    'Install apt packages' \
+        thirdparty  'Install downloaded packages' \
+        nonapt      'Install non-apt packages' \
+        configure   'Configure system' \
+        cleanup     'Cleanup the system' \
+        3>&1 1>&2 2>&3)
+     
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        $MAIN
+    else
+        quit
+    fi
+}
+
+# Quit
+function quit {
+    #if (whiptail --title "Quit" --yesno "Are you sure you want quit?" 10 60) then
+    #    echo "Exiting..."
+    #    show_info 'Tschüß!'
+    #    exit 99
+    #else
+    #    main
+    #fi
+    show_info 'Tschüß!'
+    exit 99
+}
+
+#RUN
+check_dependencies
+while :
+do
+  main
+done
+
+#END OF SCRIPT
